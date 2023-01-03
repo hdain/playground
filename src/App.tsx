7f64a031824f0f28a86d75a7e04b40a045a8f7f5
dani <85476608+dain9812@users.noreply.github.com>
2023-01-03 10:08:58 +0900

    clear git cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      This is my playground!
-    </div>
+    <Home />
   );
 }
 
