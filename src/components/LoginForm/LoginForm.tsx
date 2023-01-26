3cec8e2e6dbaec5b3e5f21d019a143f4bdf04910
hamdain <dain9812@naver.com>
2023-01-26 21:40:34 +0900

    feat: Add login func

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,58 @@
 import styles from "./LoginForm.module.scss";
 import classNames from "classnames/bind";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, values.email, values.password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className={cx("login-form")}>
+    <form className={cx("login-form")} onSubmit={handleSubmit} method="POST">
       <div className={cx("form-control")}>
-        <label htmlFor="id">ID</label>
-        <input type="text" name="id" />
+        <label htmlFor="email">ID</label>
+        <input
+          type="text"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
       </div>
       <div className={cx("form-control")}>
         <label htmlFor="password">PASSWORD</label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
       </div>
       <div className={cx("btn-wrap")}>
-        <button>LOGIN</button>
+        <button type="submit">LOGIN</button>
       </div>
     </form>
   );
