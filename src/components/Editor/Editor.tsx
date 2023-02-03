3ab2ce24b8e3509e642fec860a535efda3a4a922
hamdain <dain9812@naver.com>
2023-02-03 22:42:21 +0900

    feat: Update write page

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -6,16 +6,34 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const Editor = () => {
+  const [title, setTitle] = useState("");
   const [value, setValue] = useState<string | undefined>("**Contents...**");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
 
   return (
     <div className={cx("editor")}>
+      <div className={cx("title")}>
+        <input
+          type="text"
+          placeholder="Title..."
+          value={title}
+          onChange={handleChange}
+        />
+      </div>
+
       <MDEditor
         value={value}
         onChange={setValue}
-        height={500}
-        style={{ boxShadow: "none" }}
+        style={{ minHeight: "85vh", boxShadow: "none" }}
       />
+
+      <div className={cx("button-wrap")}>
+        <button>back</button>
+        <button>save</button>
+      </div>
     </div>
   );
 };
