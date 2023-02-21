f3a624991d82e483ed7bd0dd705458908c3383a3
dani <dain9812@naver.com>
2023-02-21 19:28:32 +0900

    feat: Get posts from firebase

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,14 +1,51 @@
 import styles from "./PostList.module.scss";
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
+import { database } from "../../firebase";
+import { child, get, ref } from "firebase/database";
 
 const cx = classNames.bind(styles);
 
+type PostType = {
+  title: string;
+  contents: string;
+  timestamp: number;
+};
+
+type PostListType = {
+  [id: string]: PostType;
+};
+
 const PostList = () => {
+  const [postList, setPostList] = useState<PostListType | undefined>(undefined);
+
+  useEffect(() => {
+    const getPostList = async () => {
+      try {
+        const response = await get(child(ref(database), "posts"));
+        if (response.exists()) {
+          const data = response.val();
+          setPostList(data);
+        } else {
+          console.log("No data available");
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    getPostList();
+  }, []);
+
   return (
     <div className={cx("post-list")}>
-      <p>post1</p>
-      <p>post2</p>
-      <p>post3</p>
+      {postList &&
+        Object.keys(postList).map((post) => (
+          <div className={cx("post")}>
+            <h3>{postList[post].title}</h3>
+            <div>{postList[post].contents}</div>
+          </div>
+        ))}
     </div>
   );
 };
