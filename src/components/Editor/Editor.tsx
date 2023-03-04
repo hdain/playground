f03787dd8d1f2e6744538a9d9da61ec27ceecda0
hamdain <dain9812@naver.com>
2023-03-04 20:11:36 +0900

    refactor: Use props and hooks

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,38 +2,31 @@ import { useEffect, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import styles from "./Editor.module.scss";
 import classNames from "classnames/bind";
-import { ref, set, serverTimestamp, get, child } from "firebase/database";
-import { database } from "../../firebase";
-import { useNavigate, useParams } from "react-router-dom";
-import translatePostTitleToPath from "../../utils/translatePostTitleToPath";
+import { useNavigate } from "react-router-dom";
+import { Post } from "../../hooks";
 
 const cx = classNames.bind(styles);
 
-const Editor = () => {
-  const { postTitle } = useParams();
+type EditorProps = {
+  isLoading?: boolean;
+  post?: Post;
+  onSave: (title: string, contents?: string) => Promise<void>;
+};
+
+const Editor = (props: EditorProps) => {
+  const { isLoading, post, onSave } = props;
+
   const navigate = useNavigate();
 
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState<string>("");
   const [contents, setContents] = useState<string | undefined>("");
 
   useEffect(() => {
-    const getPost = async () => {
-      try {
-        const snapshot = await get(child(ref(database), `posts/${postTitle}`));
-        if (snapshot.exists()) {
-          const { title, contents } = snapshot.val();
-          setTitle(title);
-          setContents(contents);
-        } else {
-          console.log("No data available");
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
-    getPost();
-  }, [postTitle]);
+    if (post) {
+      setTitle(post.title);
+      setContents(post.contents);
+    }
+  }, [post]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -42,27 +35,29 @@ const Editor = () => {
   const handleBackClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions,no-restricted-globals
-    confirm("Do you really want to leave?") ? navigate(-1) : "";
+    navigate(-1);
   };
 
   const handleSaveClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    try {
-      const titleToPath = translatePostTitleToPath(title);
-      await set(ref(database, `posts/${titleToPath}`), {
-        path: titleToPath,
-        title,
-        contents,
-        timestamp: serverTimestamp(),
-      });
+    if (!title) {
+      alert("Title is required!");
+      return;
+    }
 
+    try {
+      await onSave(title, contents);
       alert("complete 👏");
       navigate("/post");
     } catch (e) {
       console.error(e);
     }
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={cx("editor")}>
