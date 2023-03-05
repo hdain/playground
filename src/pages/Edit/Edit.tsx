5fc304136a0afe992d46fcb976f2a246a14b5640
hamdain <dain9812@naver.com>
2023-03-05 23:19:09 +0900

    feat: Add props to editor

--- src/pages/Edit/Edit.tsx
+++ src/pages/Edit/Edit.tsx
@@ -1,13 +1,36 @@
 import styles from "./Edit.module.scss";
 import classNames from "classnames/bind";
+import { useParams } from "react-router-dom";
+import { child, ref, update } from "firebase/database";
+import { database } from "../../firebase";
+import { usePost } from "../../hooks";
+import translatePostTitleToPath from "../../utils/translatePostTitleToPath";
 import { Editor } from "../../components/Editor";
 
 const cx = classNames.bind(styles);
 
-const Edit = () => (
-  <div className={cx("edit")}>
-    <Editor />
-  </div>
-);
+const Edit = () => {
+  const { postKey } = useParams();
+  const { isLoading, post } = usePost(postKey);
+
+  const handleUpdate = async (title: string, contents?: string) => {
+    try {
+      const titleToPath = translatePostTitleToPath(title);
+      await update(child(ref(database), `posts/${postKey}`), {
+        path: titleToPath,
+        title,
+        contents,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className={cx("edit")}>
+      <Editor isLoading={isLoading} post={post} onSave={handleUpdate} />
+    </div>
+  );
+};
 
 export default Edit;
--- src/pages/Write/Write.tsx
+++ src/pages/Write/Write.tsx
@@ -1,13 +1,32 @@
 import styles from "./Write.module.scss";
 import classNames from "classnames/bind";
+import { child, push, ref, serverTimestamp, set } from "firebase/database";
+import { database } from "../../firebase";
+import translatePostTitleToPath from "../../utils/translatePostTitleToPath";
 import { Editor } from "../../components/Editor";
 
 const cx = classNames.bind(styles);
 
-const Write = () => (
-  <div className={cx("write")}>
-    <Editor />
-  </div>
-);
+const Write = () => {
+  const handleCreate = async (title: string, contents?: string) => {
+    try {
+      const titleToPath = translatePostTitleToPath(title);
+      await set(push(child(ref(database), `posts`)), {
+        path: titleToPath,
+        title,
+        contents,
+        timestamp: serverTimestamp(),
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className={cx("write")}>
+      <Editor onSave={handleCreate} />
+    </div>
+  );
+};
 
 export default Write;
