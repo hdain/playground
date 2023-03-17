2f6a837eada197c9d9a9fb9150cdbbbc4187f5b9
dani <dain9812@naver.com>
2023-03-17 18:31:23 +0900

    feat: Add TagList component

---
+++ src/components/TagList/TagList.tsx
@@ -0,0 +1,22 @@
+import styles from "./TagList.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+type TagListProps = {
+  tags: Array<string>;
+};
+
+const TagList = (props: TagListProps) => {
+  const { tags } = props;
+
+  return (
+    <div className={cx("tag-list")}>
+      {tags.map((tag) => (
+        <span key={tag}>{tag}</span>
+      ))}
+    </div>
+  );
+};
+
+export default TagList;
