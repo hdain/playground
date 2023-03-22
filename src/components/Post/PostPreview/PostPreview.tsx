57e05753436c834ad77b4c28da5c9eaa147be294
dani <dain9812@naver.com>
2023-03-22 22:48:54 +0900

    feat: Update PostPreview

--- src/components/Post/PostPreview/PostPreview.tsx
+++ src/components/Post/PostPreview/PostPreview.tsx
@@ -1,18 +1,24 @@
 import styles from "./PostPreview.module.scss";
 import classNames from "classnames/bind";
 import { Post } from "../../../hooks";
+import { TagList } from "../../TagList";
+import { dateFormat } from "../../../utils";
+
+const removeMd = require("remove-markdown");
 
 const cx = classNames.bind(styles);
 
-type PostPreviewProps = Pick<Post, "title" | "contents">;
+type PostPreviewProps = Pick<Post, "title" | "tags" | "contents" | "timestamp">;
 
 const PostPreview = (props: PostPreviewProps) => {
-  const { title, contents } = props;
+  const { title, tags, contents, timestamp } = props;
 
   return (
     <article className={cx("post-preview")}>
+      <small>{dateFormat(timestamp)}</small>
       <h3>{title}</h3>
-      <p>{contents}</p>
+      <p>{removeMd(contents)}</p>
+      <TagList tags={tags} />
     </article>
   );
 };
